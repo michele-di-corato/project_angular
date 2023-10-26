04299f1a34fa17f0d9d8b4a19e01b745ab2fbf37
Michele Di Corato <micheled.46@hotmail.it>
2023-10-26 09:06:18 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'final-project',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
